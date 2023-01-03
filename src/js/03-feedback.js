99455cbd896894e0ca6d1b8417b7802fecf9eaef
AliceChpmnk <alisablog50@gmail.com>
2023-01-04 00:53:35 +0200

    edits after first check

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,36 +1,19 @@
 var throttle = require('lodash.throttle');
 
+const LOCALSTORAGE_KEY = 'feedback-form-state';
 const formEl = document.querySelector(".feedback-form");
-const userData = {};
-const userDatafromLS = JSON.parse(localStorage.getItem('feedback-form-state'));
-
-/*Я додала цю функцію, тому що при оновленні сторінки змінна userData стає пустим об'єктом, тобто втрачає властивості, які були записані раніше.
-При наступному виклику onFormChange (якщо змінити один із інпутів вже після оновлення сторінки) 
-змінна 'feedback-form-state' у локальнму сховищі перезаписується повністю.
-Тому якщо змінити лише один інпут, на цьому етапі втрачається значення іншого інпуту. 
-Я намагалась це виправити у самій функції onFormChange, але не працювало. Змогла реалізувати лише в такий спосіб:
-При оновленні сторінки оновлювати значення пустого об'єкту userData тими вастивостями, які є в локальному сховищі*/
-const matchUserData = () => {
-    if (userDatafromLS === null) {
-            return;
-    }
-    
-    for (const prop in userDatafromLS) {
-        userData[prop] = userDatafromLS[prop];
-        }
-}
 
 const fillFormFields = () => {
     try {
+        let userDataFromLS = localStorage.getItem(LOCALSTORAGE_KEY);
         
-        if (userDatafromLS === null) {
-            return;
-        }
+        if (userDataFromLS) {
+            userDataFromLS = JSON.parse(userDataFromLS);
 
-        for (const prop in userDatafromLS) {
-        formEl.elements[prop].value = userDatafromLS[prop];
+            Object.entries(userDataFromLS).forEach(([name, value]) => {
+                formEl.elements[name].value = value;
+            });
         }
-
     } catch (err) {
         console.log(err);
     }
@@ -39,23 +22,33 @@ const fillFormFields = () => {
 const onFormChange = event => {
     const { target } = event;
 
-    const keyName = target.name;
-    const value = target.value;
+    try {
+        let userDataFromLS = localStorage.getItem(LOCALSTORAGE_KEY);
+        userDataFromLS = userDataFromLS ? JSON.parse(userDataFromLS) : {};
+        
+        userDataFromLS[target.name] = target.value;
+        localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(userDataFromLS));
 
-    userData[keyName] = value;
-
-    localStorage.setItem('feedback-form-state', JSON.stringify(userData));
+    } catch(err) {
+        console.log(err);
+    }
 }
 
 const onFormSubmit = event => {
   event.preventDefault();
 
     formEl.reset();
-    console.log(userData);
-    localStorage.removeItem('feedback-form-state');
+
+    try {
+        const userDataFromLS = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
+        console.log(userDataFromLS);
+    } catch (err) {
+        console.log(err);
+    }
+    
+    localStorage.removeItem(LOCALSTORAGE_KEY);
 };
 
-matchUserData();
 fillFormFields();
 formEl.addEventListener('change', throttle(onFormChange, 500));
 formEl.addEventListener('submit', onFormSubmit);
